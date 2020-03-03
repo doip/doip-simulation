f94806dbf169ea93e3fcdc23d2a60b193ee4ea55
Marco Wehnert <marco.wehnert@gmx.de>
2020-03-03 17:15:15 +0100

    Issue #27: Implementedsupport for functional addressing.

---
+++ src/test/java/doip/simulation/TesterConfig.java
@@ -0,0 +1,17 @@
+package doip.simulation;
+
+import java.io.IOException;
+
+import doip.library.properties.EmptyPropertyValue;
+import doip.library.properties.MissingProperty;
+import doip.library.properties.PropertyFile;
+
+public class TesterConfig {
+
+	private PropertyFile file = null;
+	
+	public TesterConfig(String filename) throws IOException, MissingProperty, EmptyPropertyValue {
+		file = new PropertyFile(filename);
+		file.getMandatoryPropertyAsInt("maxByteArraySize.logging");
+	}
+}
